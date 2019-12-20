ee74cb61a8c1aa057da6958a9119c1d30bd16ca8
Michael Weedman <michael.weedman@gmail.com>
2019-12-20 07:59:21 -0700

    Initialization

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const errorHandler = require('./error');
+const validation = require('./middleware/validation');
+const gameRouter = require('./game/routes.game');
+
+const app = express();
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+
+app.use('/api/game', gameRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello, boilerplate!');
+});
+
+app.use(errorHandler);
+
+module.exports = app;
