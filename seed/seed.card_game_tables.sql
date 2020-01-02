339ffacd14ecc9925ad180633c244ee42744096f
Michael Weedman <michael.weedman@gmail.com>
2020-01-02 13:20:11 -0700

    Added user roles

--- seed/seed.card_game_tables.sql
+++ seed/seed.card_game_tables.sql
@@ -8,8 +8,8 @@ TRUNCATE
   accounts
   RESTART IDENTITY CASCADE;
 
-INSERT INTO accounts (user_name, user_password)
+INSERT INTO accounts (user_name, user_password, user_role)
 VALUES
-  ('michael', '$2y$12$tCNdl4HLgWAzCPAAXLUk9O7oxWJi6dyyU6.DhqvpyVTcqMFVPG06i');
+  ('michael', '$2y$12$tCNdl4HLgWAzCPAAXLUk9O7oxWJi6dyyU6.DhqvpyVTcqMFVPG06i','user');
 
 COMMIT;
