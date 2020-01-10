689ae0f05407b8ef7adeacb86d50fdcd65df5ed7
Michael Weedman <michael.weedman@gmail.com>
2020-01-10 07:34:19 -0700

    updated vars

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/card-game',
+  DATABASE_URL: process.env.HEROKU_DATABASE_URL,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/card-game-test',
   JWT_SECRET: process.env.JWT_SECRET || 'my-special-little-secret'
 };
